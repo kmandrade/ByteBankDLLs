6590d82a4216f436b1ecdfc6e85811da32e497dd
kelvin <kelvin@PROTECH-SSC6>
2021-12-07 09:02:41 -0300

    string regex

--- ByteBank.SistemaAgencia2/Program.cs
+++ ByteBank.SistemaAgencia2/Program.cs
@@ -2,9 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using ByteBank.Modelos2;
-
 namespace ByteBank.SistemaAgencia2
 
 {
@@ -18,13 +18,39 @@ namespace ByteBank.SistemaAgencia2
 
 
 
-            string urlParametros = "http://www.bytebank.com/cambio?moedaOrigem=real&moedaDestino=dolar";
-            ExtratorValorDeArgumentos extradorDeValores = new ExtratorValorDeArgumentos(urlParametros);
-            Console.WriteLine(extradorDeValores.GetValor("moedaDestino"));
 
+
+
+
+            
+
+
+
+            
 
 
             Console.ReadLine();
         }
+
+        //static void TetandoRegex()
+        //{
+        //    ////testando regex
+        //    //string padrao = "[0-9]{4,5}-?[0-9]{4}";
+
+        //    //string textoDeTeste = "asdadas 91234-1234";
+        //    //Match resultado = Regex.Match(textoDeTeste, padrao);
+
+        //    //Console.WriteLine(resultado.Value);
+
+        //}
+
+        //static void ExtratorArgumentos()
+        //{
+        //    //string urlParametros = "http://www.bytebank.com/cambio?moedaOrigem=real&moedaDestino=dolar";
+        //    //ExtratorValorDeArgumentos extradorDeValores = new ExtratorValorDeArgumentos(urlParametros);
+        //    //Console.WriteLine(extradorDeValores.GetValor("moedaDestino"));
+
+        //}
     }
+    
 }
